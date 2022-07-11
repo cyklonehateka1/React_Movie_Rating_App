24f363ff658de5b62639e4aeb2438f914bfe4e73
Emmanuel Hateka <emmanuelhateka.com>
2022-07-11 22:54:06 +0000

    Fetched data from from API

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import hero from "../../img/hero.png";
+import "./Header.scss";
 
 const header = () => {
-  return <div>header</div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">C.MOVIES</div>
+      </Link>
+      <div className="user-image">
+        <img src={hero} alt="user" />
+      </div>
+    </div>
+  );
 };
 
 export default header;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
+import MovieListing from "../../components/movieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
-  return <div>home</div>;
+  const movieText = "Harry";
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => console.log("Error Message:", err));
+      console.log("API response:", response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
