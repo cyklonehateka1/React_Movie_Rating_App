4b9a65de6b88821a95b28aa4fc374b8e6f53cd4b
Emmanuel Hateka <emmanuelhateka.com>
2022-07-14 22:15:13 +0000

    rendered shows data from the api to the shows section

--- src/components/movieListing/MovieListing.jsx
+++ src/components/movieListing/MovieListing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../movieCard/MovieCard";
 import "./MovieListing.scss";
 
@@ -20,11 +20,28 @@ const MovieListing = () => {
       </div>
     );
 
+  const shows = useSelector(getAllShows);
+
+  let renderShows = "";
+
+  renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) => <MovieCard key={index} data={show} />)
+    ) : (
+      <div className="movies-error">
+        <h3>{shows.Error}</h3>
+      </div>
+    );
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="movie-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,18 @@
 import { useEffect } from "react";
 import MovieListing from "../../components/movieListing/MovieListing";
-import MovieApi from "../../common/apis/MovieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux/es/exports";
-import { addMovies } from "../../features/movies/movieSlice";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await MovieApi.get(
-        `?apiKey=${APIKey}&s=${movieText}&type=movie`
-      ).catch((err) => console.log("Error Message:", err));
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
 
   return (
     <div>
